0727237ad828295a585eb1c84b81524e479e1a23
Eduardo Ernesto Corrales Reyna <desenvolvimento1@credencieconsultas.com.br>
2024-07-09 20:45:05 -0300

    feat: create router

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { CreateTrip } from "./pages/create-trip";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateTrip />,
+  },
+]);
+
 export function App() {
-  return <CreateTrip />;
+  return <RouterProvider router={router} />;
 }
