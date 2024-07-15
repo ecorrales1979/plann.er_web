958c260ee306bb95ed9830fef72085f48bccbf30
Eduardo Ernesto Corrales Reyna <desenvolvimento1@credencieconsultas.com.br>
2024-07-15 08:04:35 -0300

    feat: add modal to create new links

---
+++ src/pages/trip-details/create-link-modal.tsx
@@ -0,0 +1,57 @@
+import { Link2, Tag } from "lucide-react";
+import { FormEvent } from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "../../components/button";
+import { Modal } from "../../components/modal";
+import { api } from "../../lib/axios";
+
+type Props = {
+  onClose: () => void;
+};
+
+export function CreateLinkModal({ onClose }: Props) {
+  const { tripId } = useParams();
+
+  const createLink = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const title = data.get("title");
+    const url = data.get("url");
+    api.post(`/trips/${tripId}/links`, { title, url }).then(() => {
+      onClose();
+    });
+  };
+
+  return (
+    <Modal
+      title="Register link"
+      subtitle="All guests can see the important links."
+      onClose={onClose}
+    >
+      <form onSubmit={createLink} className="space-y-3">
+        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+          <Tag className="size-5 text-zinc-400" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Link title"
+            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+          />
+        </div>
+        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 flex-1">
+          <Link2 className="size-5 text-zinc-400" />
+          <input
+            type="url"
+            name="url"
+            placeholder="URL"
+            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+          />
+        </div>
+        <Button type="submit" color="primary" size="full">
+          Save link
+        </Button>
+      </form>
+    </Modal>
+  );
+}
--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,9 +1,17 @@
 import { Link2, Plus } from "lucide-react";
+import { useState } from "react";
 import { Button } from "../../components/button";
+import { CreateLinkModal } from "./create-link-modal";
 
 // type Props = {}
 
 export function ImportantLinks() {
+  const [isCreateLinkModalOpen, setIsCreateLinkModalOpen] = useState(false);
+
+  const openCreateLinkModal = () => setIsCreateLinkModalOpen(true);
+
+  const closeCreateLinkModal = () => setIsCreateLinkModalOpen(false);
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Important links</h2>
@@ -37,10 +45,13 @@ export function ImportantLinks() {
           <Link2 className="size-5 text-zinc-400 shrink-0" />
         </div>
       </div>
-      <Button size="full">
+      <Button onClick={openCreateLinkModal} size="full">
         <Plus className="size-5" />
         Add new link
       </Button>
+      {isCreateLinkModalOpen && (
+        <CreateLinkModal onClose={closeCreateLinkModal} />
+      )}
     </div>
   );
 }
