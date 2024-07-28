b4b457fc10fd37981c29134394f562dc2679bc6d
Eduardo Ernesto Corrales Reyna <desenvolvimento1@credencieconsultas.com.br>
2024-07-28 00:46:23 -0300

    test: add test for formatDateAndMonthRange function

--- src/utils/format.spec.ts
+++ src/utils/format.spec.ts
@@ -1,15 +1,58 @@
-import { formatDateAndMonth } from './format';
+import { formatDateAndMonth, formatDateAndMonthRange } from './format';
 
-describe('Testing formatDateAndMonthFunction', () => {
+const date1Str = '2024-01-01';
+const date1 = new Date(date1Str);
+const date2Str = '2024-01-20';
+const date2 = new Date(date2Str);
+
+describe('Testing formatDateAndMonth function', () => {
   it('should return formatted date and month from Date', () => {
-    const date = new Date('2024-01-01');
-    const formattedDate = formatDateAndMonth(date);
+    // const date = new Date('2024-01-01');
+    const formattedDate = formatDateAndMonth(date1);
     expect(formattedDate).toBe('Jan, 1st');
   });
 
   it('should return formatted date and month from string', () => {
-    const date = '2024-01-01';
-    const formattedDate = formatDateAndMonth(date);
+    const formattedDate = formatDateAndMonth(date1Str);
+    expect(formattedDate).toBe('Jan, 1st');
+  });
+});
+
+describe('Testing formatDateAndMonthRange function', () => {
+  it('should return formatted date and month range from Date', () => {
+    const formattedDate = formatDateAndMonthRange({ from: date1, to: date2 });
+    expect(formattedDate).toBe('Jan, 1st to Jan, 20th');
+  });
+
+  it('should return formatted date and month range from string', () => {
+    const formattedDate = formatDateAndMonthRange({
+      from: date1Str,
+      to: date2Str,
+    });
+    expect(formattedDate).toBe('Jan, 1st to Jan, 20th');
+  });
+
+  it('should return empty string when one of range dates is missing', () => {
+    const formattedDate = formatDateAndMonthRange({
+      to: '',
+      from: date1Str,
+    });
+    expect(formattedDate).toBe('');
+  });
+
+  it('should order range correctly when from date is grater than to date', () => {
+    const formattedDate = formatDateAndMonthRange({
+      from: date2Str,
+      to: date1Str,
+    });
+    expect(formattedDate).toBe('Jan, 1st to Jan, 20th');
+  });
+
+  it('should return a single date when dates in range are equals', () => {
+    const formattedDate = formatDateAndMonthRange({
+      from: date1Str,
+      to: date1Str,
+    });
     expect(formattedDate).toBe('Jan, 1st');
   });
 });
